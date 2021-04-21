077c1c6310f934c9269636e9fc81f0f76851102e
jasminMerchant <jasmin@wlc.technology>
2021-04-22 00:34:29 +0530

    Fixed copy to clipboard issue

--- client/src/components/Atoms/CopyToClipboard.tsx
+++ client/src/components/Atoms/CopyToClipboard.tsx
@@ -1,6 +1,42 @@
 import React, { useState } from 'react'
-import copy from 'copy-to-clipboard'
 import { Button } from '@blueprintjs/core'
+
+const copy = (text: string) => {
+  let success = false
+  try {
+    // Create container for the HTML
+    const container = document.createElement('div')
+    container.innerHTML = text
+
+    // Hide element
+    container.style.position = 'fixed'
+    container.style.pointerEvents = 'none'
+    container.style.opacity = '0'
+
+    // Mount the container to the DOM to make `contentWindow` available
+    document.body.appendChild(container)
+
+    // Copy to clipboard
+    window.getSelection()!.removeAllRanges()
+
+    const range = document.createRange()
+    range.selectNode(container)
+    window.getSelection()!.addRange(range)
+
+    const successful = document.execCommand('copy')
+    if (!successful) {
+      throw new Error('copy command was unsuccessful')
+    }
+    success = true
+
+    // Remove the container
+    document.body.removeChild(container)
+  } catch (err) {
+    console.log(err)
+    success = false
+  }
+  return success
+}
 
 const CopyToClipboard = ({ getText }: { getText: () => string }) => {
   const [copied, setCopied] = useState(false)
@@ -8,7 +44,7 @@ const CopyToClipboard = ({ getText }: { getText: () => string }) => {
     <Button
       icon={copied ? 'tick-circle' : 'clipboard'}
       onClick={() => {
-        const success = copy(getText(), { format: 'text/html' })
+        const success = copy(getText())
         if (success) {
           setCopied(true)
           setTimeout(() => setCopied(false), 3000)
